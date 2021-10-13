c8c390da9cb13d5832021dc38a0ccbda0b6294e6
ionut2592 <91825485+ionut2592@users.noreply.github.com>
2021-10-13 12:30:42 +0200

    Update Linia.kt

--- versioKotlin/KSceneMaker/app/src/main/kotlin/com/ieseljust/kscenemaker/Linia.kt
+++ versioKotlin/KSceneMaker/app/src/main/kotlin/com/ieseljust/kscenemaker/Linia.kt
@@ -30,7 +30,7 @@ class Linia: Figura {
     override fun render(g:Graphics) {
         
         /*
-         * Mètode que dibuixa sobre un context gràfic la figura el·lipse.
+         * Mètode que dibuixa sobre un context gràfic la "figura" linia.
          * S'utilitza per al mètode render() de App.
          * 
          * Per dibuixar altres primitives de la classe Graphics, podeu consultar
@@ -42,7 +42,7 @@ class Linia: Figura {
          */
 
         g.setColor(color)             // Establim el color interior
-        g.drawLine (x, y, x2,y2)    // Dibuixem un el·lipse en la posició i mida indicades
+        g.drawLine (x, y, x2,y2)    // Dibuixem una linia en la posició i mida indicades
     }
 
 }
